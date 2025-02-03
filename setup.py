1eafcdb557218a23245f2882ec15b8e1c81ca0da
svetozarov <126297582+svetozarov@users.noreply.github.com>
2025-02-03 18:36:29 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="AS_ComfyGPT",
+    version="0.1.0",
+    description="A ComfyUI plugin that integrates ChatGPT using the OpenAI API.",
+    author="Artem Svetozarov",
+    author_email="art.svetozarov@gmail.com",
+    url="https://github.com/svetozarov/AS_ComfyGPT",
+    packages=find_packages(),
+    install_requires=[
+        "openai>=0.27.0"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
